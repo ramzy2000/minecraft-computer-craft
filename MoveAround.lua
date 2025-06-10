25870ac28753ba1fb6cb258ac260c8e739cb52e8
cduffy-tri <caseyduffy00@gmail.com>
2025-06-09 23:43:25 -0500

    create controller class

--- MoveAround.lua
+++ MoveAround.lua
@@ -1,4 +1,16 @@
-function findDirection()
+-- Meta class
+Controller = { isRunning = true }
+
+-- Derived class method newproxy
+function Controller:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    self.isRunning = true
+    return o
+end
+
+function Controller:findDirection()
     if turtle.detect() then
         directionFound = false
         while not directionFound do
@@ -10,20 +22,20 @@ function findDirection()
     end
 end
 
-function run()
-    isAwake = true
-    while isAwake do
+function Controller:run()
+    while self.isRunning do
         -- check if the turtle is out of fuel
         if turtle.getFuelLevel() < 2 then
             turtle.refuel()
         end
 
         -- find the direction we want to move
-        findDirection()
+        self:findDirection()
 
         -- move the turtle
         turtle.forward()
     end
 end
 
-run()
+controller = Controller:new(nil)
+controller:run()
