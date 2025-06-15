1b67d1a1913c69837bd04a3b96a40508672efe1e
cduffy-tri <caseyduffy00@gmail.com>
2025-06-14 19:13:51 -0500

    Clean up EntityGlobalPos

--- EntityGlobalPos.lua
+++ EntityGlobalPos.lua
@@ -1,53 +1,78 @@
-EntityGlobalPos = {}
+local EntityGlobalPos = {}
 
 function EntityGlobalPos:new(o, filePath, initPos)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
+
+    if not filePath or filePath == "" then
+        error("File path cannot be empty")
+    end
     o.filePath = filePath
 
-    if filePath == "" then
-        return
-    end
-
-    -- check if file exists
-    if (not fs.exists(o.filePath)) then
-        -- if not then create one with a base pos of 0, 0, 0
-        file = fs.open(o.filePath, "w")
-        if file then
-            file.writeLine(initPos)
-            file.close()
+    -- If file does not exist, create it with the initial position
+    if not fs.exists(o.filePath) then
+        local file = fs.open(o.filePath, "w")
+        if not file then
+            error("Failed to create file at: " .. o.filePath)
         end
+        file.writeLine(initPos or "0,0,0")
+        file.close()
     end
 
     return o
 end
 
 function EntityGlobalPos:setPos(entityPos)
-    -- convert the position into a string
-    result = entityPos.x .. "," .. entityPos.y .. "," .. entityPos.z
-    -- write this position to the file
-    file = fs.open(self.filePath, "w+")
+    -- Validate input
+    if not entityPos or type(entityPos) ~= "table"
+        or not entityPos.x or not entityPos.y or not entityPos.z then
+        error("Invalid position data provided to setPos")
+    end
+    local result = entityPos.x .. "," .. entityPos.y .. "," .. entityPos.z
+
+    local file = fs.open(self.filePath, "w+")
+    if not file then
+        error("Failed to open file for writing: " .. self.filePath)
+    end
     file.write(result)
     file.close()
 end
 
 function EntityGlobalPos:getPos()
-    pos = vector.new(0, 0, 0)
-    file = fs.open(self.filePath, "r")
-    content = ""
-    if file then
-        content = file.readAll()
+    local pos = vector.new(0, 0, 0)
+    local file = fs.open(self.filePath, "r")
+    if not file then
+        error("Failed to open file for reading: " .. self.filePath)
     end
-    numbers = {}
-    -- parse out the text into the numbers
+    local content = file.readAll()
+    file.close()
+    local numbers = {}
+
+    -- Parse the stored string into coordinates
     for num in string.gmatch(content, "[^,]+") do
         table.insert(numbers, tonumber(num))
     end
-    pos.x = numbers[1]
-    pos.y = numbers[2]
-    pos.z = numbers[3]
+
+    if #numbers == 3 then
+        pos.x = numbers[1]
+        pos.y = numbers[2]
+        pos.z = numbers[3]
+    else
+        error("Corrupted position data in file: " .. self.filePath)
+    end
+
     return pos
+end
+
+function EntityGlobalPos:exists()
+    return fs.exists(self.filePath)
+end
+
+function EntityGlobalPos:delete()
+    if fs.exists(self.filePath) then
+        fs.delete(self.filePath)
+    end
 end
 
 return EntityGlobalPos
